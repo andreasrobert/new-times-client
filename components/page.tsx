1d05ff3005479499af908824f6aaa01b28513d5a
andreasrobert <andreasrobb23@gmail.com>
2021-08-11 16:33:33 +0700

    layout

--- components/page.tsx
+++ components/page.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Text, GridItem, Grid } from "@chakra-ui/react";
+import { Flex, Heading, Text, GridItem, Grid, Box } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 import styled from "styled-components";
@@ -24,57 +24,65 @@ function Page() {
         <Heading size="H2">
           Woman spends to much time on Codepens despite advice; no plans to stop
         </Heading>
-        <Text size="p">
-          Sometimes, Internet users land themselves in an uncommon situation
-          where an app or website can be used for both work and personal
-          situations. Well, a young front-end designer was spending up to 13
-          hours on the site, Codepen for work and her hobby, which
-          unfortunately, is also coding. The designer, whose work includes a
-          To-Do Terrarium, clarified, Well, I didnt have to use it for work but
-          it was already part of my workflow. It just made sense as it was more
-          efficient. The biggest issue, she said, is that she would hop on
-          Codepen for work, but would get distracted by pens made by others in
-          the community for unexpectedly long periods of time, which then causes
-          her to spend even more time on the site. I mean, thats terrible right?
-          she asked. Codepen, which brands itself as the best place to build,
-          test, and discover front-end code, has declined to comment at this
+        <Box 
+        my="20px"
+        w="50%"
+        textAlign="justify"
+        sx={{
+            columnCount: { base: "1", ts: "2"},
+            columnGap: "30px",   
+        }}
+        >
+        <Text  size="p">
+        Sometimes, Internet users land themselves in an uncommon situation where an app or website can be used for both work and personal situations. Well, a young front-end designer was spending up to 13 hours on the site, Codepen for work and her hobby, which unfortunately, is also coding.
           time.
         </Text>
+        <Text mt="20px" size="p">
+        The designer, whose work includes a To-Do Terrarium, clarified, Well, I didnt have to use it for work but it was already part of my workflow. It just made sense as it was more efficient. The biggest issue, she said, is that she would hop on Codepen for work, but would get distracted by pens made by others in the community for unexpectedly long periods of time, which then causes her to spend even more time on the site.
+        </Text>
+        <Text mt="20px" size="p">
+        I mean, thats terrible right? she asked. Codepen, which brands itself as the best place to build, test, and discover front-end code, has declined to comment at this time.
+        </Text>
+        </Box>
       </Flex>
+
+
 
       <Grid
   h="200px"
   w="70vw"
   templateRows="repeat(2, 1fr)"
-  templateColumns="repeat(7, 1fr)"
   gap={4} 
   mb="200px"
 >
-  <GridItem colSpan={7} bg="tomato">
-  <Heading size="H2">
+  <Heading size="H2" w="150">
           Woman spends to much time on Codepens despite advice; no plans to stop
         </Heading>
-  </GridItem>
-  <GridItem bg="papayawhip">
-  <Text size="p">
-          Sometimes, Internet users land themselves in an uncommon situation
-          where an app or website can be used for both work and personal
-          situations. Well, a young front-end designer was spending up to 13
-          hours on the site, Codepen for work and her hobby, which
-          unfortunately, is also coding. The designer, whose work includes a
-          To-Do Terrarium, clarified, Well, I didnt have to use it for work but
-          it was already part of my workflow. It just made sense as it was more
-          efficient. The biggest issue, she said, is that she would hop on
-          Codepen for work, but would get distracted by pens made by others in
-          the community for unexpectedly long periods of time, which then causes
-          her to spend even more time on the site. I mean, thats terrible right?
-          she asked. Codepen, which brands itself as the best place to build,
-          test, and discover front-end code, has declined to comment at this
+  <GridItem border="1px solid black">
+  <Box 
+        my="20px"
+        w="50%"
+        sx={{
+            columnCount: { base: "1", ts: "2"},
+            columnGap: "30px",   
+        }}
+        textAlign="justify"
+        >
+        <Text  size="p">
+        Sometimes, Internet users land themselves in an uncommon situation where an app or website can be used for both work and personal situations. Well, a young front-end designer was spending up to 13 hours on the site, Codepen for work and her hobby, which unfortunately, is also coding.
           time.
         </Text>
+        <Text mt="5px" size="p">
+        The designer, whose work includes a To-Do Terrarium, clarified, Well, I didnt have to use it for work but it was already part of my workflow. It just made sense as it was more efficient. The biggest issue, she said, is that she would hop on Codepen for work, but would get distracted by pens made by others in the community for unexpectedly long periods of time, which then causes her to spend even more time on the site.
+        </Text>
+        <Text mt="5px" size="p">
+        I mean, thats terrible right? she asked. Codepen, which brands itself as the best place to build, test, and discover front-end code, has declined to comment at this time.
+        </Text>
+        </Box>
   </GridItem>
-  <GridItem colSpan={2} bg="papayawhip" />
 </Grid>
+
+
 
     </>
   );
